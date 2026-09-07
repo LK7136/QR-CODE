6c882179c53ac584c64db1781ad0e3a4b3359d26
LK7136 <lounes.kissoum1@gmail.com>
2026-09-07 16:15:10 +0200

    ajout de la fonction local storage

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
-      const nombre = Math.floor(Math.random() * 10001); 
-      document.getElementById('resultat').textContent = 
-        "Ton numero est : " + nombre;
+
+      let nombre = localStorage.getItem('numeroUtilisateur');
+      
+      if(!nombre) {
+        nombre = Math.floor(Math.random() * 10001); 
+        localStorage.setItem('numeroUtilisateur', nombre);
+      }
+
+      document.getElementById('resultat').textContent = "Ton numero est : " + nombre;
+
     });
